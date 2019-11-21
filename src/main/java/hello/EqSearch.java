f8b20e0b0b452242ae608946f42a7b1dc54e0d91
Ruxin Zhang <35670612+ruxin827@users.noreply.github.com>
2019-11-20 22:29:12 -0800

    rz - create form

---
+++ src/main/java/hello/EqSearch.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class EqSearch{
+	private int distance;
+	private int minmag;
+
+	public int getDistance(){
+		return distance;
+	}
+
+	public int getMinmag(){
+		return minmag;
+	}
+
+	public void setDistance(int d){
+		this.distance = d;
+	}
+
+	public void setMinmag(int m){
+		this.minmag = m;
+	}
+}
