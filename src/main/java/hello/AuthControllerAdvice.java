e8fb2f582b24d7e5580a3121a23da1ab5cb2da2e
Ruxin Zhang <35670612+ruxin827@users.noreply.github.com>
2019-11-26 17:57:49 -0800

    add updates

--- src/main/java/hello/AuthControllerAdvice.java
+++ src/main/java/hello/AuthControllerAdvice.java
@@ -5,10 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import hello.repositories.UserRepository;
+import hello.entities.AppUser;
+import java.util.List;
 
 @ControllerAdvice
 public class AuthControllerAdvice {
 
+    @Autowired   
+    private UserRepository userRepository;
+    
     @Autowired   
     private MembershipService membershipService;
 
