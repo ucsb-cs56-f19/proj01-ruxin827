174c2f250aa7143c4851743fb277d3a01dd8db97
Ruxin Zhang <35670612+ruxin827@users.noreply.github.com>
2019-12-03 03:34:15 -0800

    rz - fix some bugs

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
